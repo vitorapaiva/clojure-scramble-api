d992c12679e294ee39e6fe480b09897305c4c206
Vitor Paiva <vitor.paiva@picpay.com>
2021-06-19 11:41:34 -0300

    Setting up the server

--- Makefile
+++
@@ -1,2 +0,0 @@
-compile:
-	- docker run -it --rm -v "$PWD":/usr/src/app -w /usr/src/app clojure lein uberjar
--- app/src/app/core.clj
+++ app/src/app/core.clj
@@ -1,6 +1,25 @@
-(ns app.core)
+(ns app.core
+    (:require [org.httpkit.server :as server]
+      [compojure.core :refer :all]
+      [compojure.route :as route]
+      [ring.middleware.defaults :refer :all]
+      [clojure.pprint :as pprint]
+      [clojure.string :as str]
+      [clojure.data.json :as json])
+    (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes app-routes
+  (GET "/scramble" [] scramble)
+  (route/not-found "Error, page not found!"))
+
+(defn -main
+  "This is our main entry point"
+  [& args]
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    ; Run the server with Ring.defaults middleware
+    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    ; Run the server without ring defaults
+    ;(server/run-server #'app-routes {:port port})
+    (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
+
+
